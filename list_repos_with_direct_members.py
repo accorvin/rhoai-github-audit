b1723cc05db0c4594df287914ce702fe82a00a8d
Alex Corvin <acorvin@redhat.com>
2024-08-26 16:11:59 -0400

    Make direct membership audit script configurable for target org

--- list_repos_with_direct_members.py
+++ list_repos_with_direct_members.py
@@ -1,14 +1,23 @@
 #! /usr/bin/env python3
 
+import argparse
+
 from github import Auth, Github
 from tqdm import tqdm
 from utils import get_github_token, get_organization_repositories, get_direct_members
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--organization', help='The name of the GitHub organization to audit',
+                        default='opendatahub-io')
+    return parser.parse_args()
+
 def main():
+    args = parse_args()
     github_token = get_github_token()
     auth = Auth.Token(github_token)
     g = Github(auth=auth)
-    repos = get_organization_repositories(g, 'opendatahub-io')
+    repos = get_organization_repositories(g, args.organization)
 
     print(f'Checking for repositories with users added directly...')
     for repo in tqdm(repos):
